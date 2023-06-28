6036e546123747d73d2ee29915c8c245bb3a9af1
Hyeonjin Jo <zxzx1825@unist.ac.kr>
2023-06-28 14:45:32 +0900

    print

--- code/testbench_ff.py
+++ code/testbench_ff.py
@@ -25,41 +25,41 @@ device = "vta"
 target = env.target if device == "vta" else env.target_vta_cpu
 
 
-if env.TARGET not in ["sim", "tsim", "intelfocl"]:
+#if env.TARGET not in ["sim", "tsim", "intelfocl"]:
 
     # Get remote from tracker node if environment variable is set.
     # To set up the tracker, you'll need to follow the "Auto-tuning
     # a convolutional network for VTA" tutorial.
-    tracker_host = os.environ.get("TVM_TRACKER_HOST", None)
-    tracker_port = os.environ.get("TVM_TRACKER_PORT", None)
+    #tracker_host = os.environ.get("TVM_TRACKER_HOST", None)
+    #racker_port = os.environ.get("TVM_TRACKER_PORT", None)
     # Otherwise if you have a device you want to program directly from
     # the host, make sure you've set the variables below to the IP of
     # your board.
-    device_host = os.environ.get("VTA_RPC_HOST", "192.168.1.152")
-    device_port = os.environ.get("VTA_RPC_PORT", "9091")
-    if not tracker_host or not tracker_port:
-        remote = rpc.connect(device_host, int(device_port))
-    else:
-        remote = autotvm.measure.request_remote(
-            env.TARGET, tracker_host, int(tracker_port), timeout=10000
-        )
+device_host = os.environ.get("VTA_RPC_HOST", "192.168.1.152")
+device_port = os.environ.get("VTA_RPC_PORT", "9091")
+    #if not tracker_host or not tracker_port:
+remote = rpc.connect(device_host, int(device_port))
+    #else:
+    #    remote = autotvm.measure.request_remote(
+    #        env.TARGET, tracker_host, int(tracker_port), timeout=10000
+    #    )
 
     # Reconfigure the JIT runtime and FPGA.
     # You can program the FPGA with your own custom bitstream
     # by passing the path to the bitstream file instead of None.
-    reconfig_start = time.time()
-    vta.reconfig_runtime(remote)
-    vta.program_fpga(remote, bitstream=None)
-    reconfig_time = time.time() - reconfig_start
-    print("Reconfigured FPGA and RPC runtime in {0:.2f}s!".format(reconfig_time))
+reconfig_start = time.time()
+vta.reconfig_runtime(remote)
+vta.program_fpga(remote, bitstream=None)
+reconfig_time = time.time() - reconfig_start
+print("Reconfigured FPGA and RPC runtime in {0:.2f}s!".format(reconfig_time))
 
-# In simulation mode, host the RPC server locally.
-else:
-    remote = rpc.LocalSession()
+# # In simulation mode, host the RPC server locally.
+# else:
+#     remote = rpc.LocalSession()
 
-    if env.TARGET in ["intelfocl"]:
-        # program intelfocl aocx
-        vta.program_fpga(remote, bitstream="vta.bitstream")
+#     if env.TARGET in ["intelfocl"]:
+#         # program intelfocl aocx
+#         vta.program_fpga(remote, bitstream="vta.bitstream")
 
 # Get execution context from remote
 ctx = remote.ext_dev(0) if device == "vta" else remote.cpu(0)
